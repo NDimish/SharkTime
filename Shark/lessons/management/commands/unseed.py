9abe3faa81d51e0ae2ce09db7b45d25f0dda8817
lazyN <62480903+NDimish@users.noreply.github.com>
2022-11-17 15:03:04 +0000

    Delete Shark directory

--- Shark/lessons/management/commands/unseed.py
+++
@@ -1,6 +0,0 @@
-from django.core.management.base import BaseCommand, CommandError
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        print("The unseed command has not been implemented yet!")
-        print("TO DO: Create an unseed command following the instructions of the assignment carefully.")
