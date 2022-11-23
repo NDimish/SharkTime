bf71e66bbfb9ebe932fa1bc6b77cded36663db60
Pepp <agamer743@gmail.com>
2022-11-23 18:06:41 +0000

    Add BankTransfer to store details of a transfer

--- lessons/models.py
+++ lessons/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
+# Aodhán
+
+"""
+A simple class to store a reference number and an amount paid
+"""
+class BankTransfer:
+    amount = 0              # The amount transferred
+    studentNumber = "0000"  # The student number used to identify which student is paying, this is 4 digits long
+    invoiceNumber = "000"   # The invoice number, this is 3 digits long
+
+
+    """
+    Initialise the BankTransfer with the amount paid, the student's number
+    and the invoice number
+    """
+    def __init__(self, amount, studentNumber, invoiceNumber):
+        self.amount = amount
+        self.studentNumber = studentNumber
+        self.invoiceNumber = invoiceNumber
+
+
+    """
+    Return the full reference number, as a string, constructed from
+    seperating the student number from the invoice number by a dash
+    """
+    def getReferenceNumber(self):
+        return f"{self.studentNumber}-{self.invoiceNumber}"
+
+
+    """
+    Return the amount paid
+    """
+    def getAmount(self):
+        return self.amount  
+
+# /Aodhán  
+
+        
+        
+
