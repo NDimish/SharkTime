b0b17fcade6e797f8a1c6a974b49d67680879107
Pepp <agamer743@gmail.com>
2022-11-23 18:07:55 +0000

    Add skeleton code for an admin confirming requests

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# Aodhán
+from .models import User
+
+"""
+Skeleton template admin class to be replaced
+"""
+@admin.register(User)
+class MusicAdmin(admin.ModelAdmin):
+    
+    """
+    Arbitrary skeleton method for confirming a student's lesson request
+    """
+    def confirm(self, request, day, teacher, start, duration, interval, number):
+        request.set(day, teacher, start, duration, interval, number)
+        booking = request.makeBooking()
+        database.addBooking(booking)
+        price = duration * number * teacher.getCost()
+        database.addInvoice(booking, price)
+
+
+
+# /Aodhán
