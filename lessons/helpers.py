f0c9226d66d666a82e39dbec6d8e29882a650e42
Sita Sethurajan <k21034097@kcl.ac.uk>
2022-11-24 15:14:24 +0000

    Add date validation method to helpers.py

---
+++ lessons/helpers.py
@@ -0,0 +1,13 @@
+#Add helper functions here
+
+
+DAY_OF_THE_WEEK = {
+    (0, 'Monday'),
+    (1, 'Tuesday'),
+    (2, 'Wednesday'),
+    (3, 'Thursday'),
+    (4, 'Friday'),
+    #For now assume weekend leesons are not possible to book 
+    #(5, 'Saturday'),
+    #(6, 'Sunday'),
+}
