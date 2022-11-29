2c9a5857923447a9fe8c6c356aa4f50fe59d7819
Sita Sethurajan <k21034097@kcl.ac.uk>
2022-11-29 04:53:12 +0000

    Remove comments

--- lessons/Urls/adminUrls.py
+++ lessons/Urls/adminUrls.py
@@ -5,8 +5,7 @@ urlpatterns =[
 
     path('home/',adminViews.admin_home),
     path('<int:id>', adminViews.view_request, name='view_request'),
-     #path('addBooking/<int:request_id>/', adminViews.make_booking, name='add_booking'),
     path('addBooking/<int:id>/', adminViews.add_booking, name = 'add_booking'),
-    #path('addBooking/', adminViews.add_booking, name = 'add_booking'),
+    
 
 ]
