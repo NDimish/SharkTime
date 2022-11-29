be29e85d1e5ac96e4661ff350a7141915487a923
Sita Sethurajan <k21034097@kcl.ac.uk>
2022-11-29 04:16:10 +0000

    Update adminUrls to add  booking urls

--- lessons/Urls/adminUrls.py
+++ lessons/Urls/adminUrls.py
@@ -4,6 +4,9 @@ from ..views import adminViews
 urlpatterns =[
 
     path('home/',adminViews.admin_home),
-    path('<int:id>', adminViews.view_request, name='view_request')
+    path('<int:id>', adminViews.view_request, name='view_request'),
+     #path('addBooking/<int:request_id>/', adminViews.make_booking, name='add_booking'),
+    path('addBooking/<int:id>/', adminViews.add_booking, name = 'add_booking'),
+    #path('addBooking/', adminViews.add_booking, name = 'add_booking'),
 
 ]
