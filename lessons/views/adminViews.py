cba0996c325776d327b6a9b361761ef4b4cdc9d0
Sita Sethurajan <k21034097@kcl.ac.uk>
2022-12-01 15:22:21 +0000

    Create views for making bookings

--- lessons/views/adminViews.py
+++ lessons/views/adminViews.py
@@ -7,6 +7,10 @@ from django.urls import reverse
 import lessons.models as models
 from lessons.forms import adminForms
 from lessons.forms.adminForms import bookingForm as BookingForm
+from ..models import bookings
+from django import forms
+from ..models import requests
+
 
 
 """
@@ -17,6 +21,7 @@ from lessons.forms.adminForms import bookingForm as BookingForm
 def admin_home(request):
     #Get all requests 
     requests = models.request.objects.all()
+
     context = {'requests' : requests}
     #request_count = models.request.objects.all().count()
     return render(request, 'adminHome.html', context)
@@ -36,37 +41,37 @@ def make_booking(request, request_id):
     booking_request = models.requests.objects.get(pk=request_id)
     print("hello")
     print(booking_request.teacher)
-    
+    form = adminForms.bookingForm(request.POST or None )
     if request.method =='POST':
         
         form = adminForms.bookingForm(request.POST)
         
         print (form.errors)
         if form.is_valid():
-
+            form.save(commit=True)
           
-            new_created_at = form.cleaned_data['created_at']
-            new_updated_at = form.cleaned_data['updated_at']
-            new_day_of_week = form.cleaned_data['day_of_week']
-            new_time_of_lesson = form.cleaned_data['time_of_lesson']
-            new_teacher = form.cleaned_data['teacher']
-            new_start_date = form.cleaned_data['start_date']
-            new_LessonDuration =  form.cleaned_data['LessonDuration']
-            new_LessonIntervals = form.cleaned_data['LessonIntervals']
-            new_number_of_lessons =  form.cleaned_data['number_of_lessons']
-        new_booking = models.booking(
-            new_created_at,
-            new_updated_at,
-            new_day_of_week,
-            new_time_of_lesson ,
-            new_teacher ,
-            new_start_date,
-            new_LessonDuration,
-            new_LessonIntervals ,
-            new_number_of_lessons 
-        )
+        #     new_created_at = form.cleaned_data['created_at']
+        #     new_updated_at = form.cleaned_data['updated_at']
+        #     new_day_of_week = form.cleaned_data['day_of_week']
+        #     new_time_of_lesson = form.cleaned_data['time_of_lesson']
+        #     new_teacher = form.cleaned_data['teacher']
+        #     new_start_date = form.cleaned_data['start_date']
+        #     new_LessonDuration =  form.cleaned_data['LessonDuration']
+        #     new_LessonIntervals = form.cleaned_data['LessonIntervals']
+        #     new_number_of_lessons =  form.cleaned_data['number_of_lessons']
+        # new_booking = models.booking(
+        #     new_created_at,
+        #     new_updated_at,
+        #     new_day_of_week,
+        #     new_time_of_lesson ,
+        #     new_teacher ,
+        #     new_start_date,
+        #     new_LessonDuration,
+        #     new_LessonIntervals ,
+        #     new_number_of_lessons 
+        # )
         #Create the new booking object
-        new_booking.save()
+        #new_booking.save()
         #mark request as fulfilled  if not already 
 
         
@@ -87,25 +92,71 @@ def make_booking(request, request_id):
         'form' : adminForms.bookingForm(request_id)
     })
 
+
+
+
 #add a booking
 def add_booking(request,id):
     corresponding_request = models.requests.request.objects.get(pk=id) 
-    form = BookingForm()
-    print(" teacher is " , corresponding_request.Teacher)
-    if request.method == 'GET':
-        form = adminForms.bookingForm(initial={'request' : id})
-    if request.method == 'POST':
+    #Get the available days 
+    form = BookingForm(request.POST or None)
+    #days=requests.request.objects.filter(pk = id).values_list('availability')
+    #Filter the day_of_week field so that only days student marked as available can be booked
+    #BookingForm.base_fields['day_of_week'] = forms.ModelMultipleChoiceField(queryset=requests.Weekday.objects.filter(pk__in=days), widget=forms.CheckboxSelectMultiple())   
+    
+
+    if 'Submit' in request.method == 'POST':
+       # booking = get_ob
         form = BookingForm(request.POST)
         print (form.errors)
         if form.is_valid():
             
             corresponding_request.status = 'A'
             corresponding_request.save()
-            form.save()
+            form.save(commit=True)
             #Redirect back to admin home page
-            print("hello")
+            
             return redirect('/')
         #print('Printing post' , request.POST)
+    elif 'Reject' in request.POST:
+        form = BookingForm(request.POST)
+        corresponding_request.status = 'R'
+        corresponding_request.save()
+        form.save(commit=True)
+        #Redirect back to admin home page
+        
+        return redirect('/')
+
+    else :
+        form = adminForms.bookingForm( initial=get_init_booking_data(id))
+    
     context ={'form' : form ,'request' : corresponding_request}
     print("return render")
     return render(request, 'adminAddBooking.html', context)
+
+"""
+Create an initial booking object based on the request
+"""
+def get_init_booking_data(id):
+    request = models.requests.request.objects.get(pk=id) 
+    initial_data = {
+        'request' : id,
+        'teacher' : request.lesson_teacher ,
+        #'time_of_lesson' : request. ,
+        'start_date' : request.lesson_start_date, 
+        #add day_of_week
+        'lesson_time' : request.lesson_time,
+        'lesson_duration' : request.lesson_duration,
+        'lesson_interval' : request.lesson_interval,
+        'lesson_type' : request.lesson_type ,
+        'number_of_lessons' : request.number_of_lessons
+
+    }
+
+    #print (request.availability)
+    return initial_data
+
+
+
+
+ 
