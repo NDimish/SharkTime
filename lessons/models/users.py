e4eb8bce9e36c9e5613417bacb4e06167b545cc8
SitaSeth <k21034097@kcl.ac.uk>
2022-12-01 18:09:24 +0000

    Remove object manager field

--- lessons/models/users.py
+++ lessons/models/users.py
@@ -14,8 +14,7 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=100)
-    objects = models.Manager()
-
+    
     def __str__(self):
         return '{}'.format(self.get_full_name())
 
