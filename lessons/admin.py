60768939ae52a1a0621021ad566a1a7b2a55d782
Sita Sethurajan <k21034097@kcl.ac.uk>
2022-12-01 15:15:31 +0000

    Update admin to register users and students

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from .models.requests import request
 from .models.bookings import booking
+
+from .models import requests
+from .models import users
 # Register your models here.
-admin.site.register(request)
+admin.site.register(request , requests.requestAdmin)
 admin.site.register(booking)
+admin.site.register(users.User)
+admin.site.register(users.Student)
+admin.site.register(requests.Weekday)
+
+
+
+
+
+
