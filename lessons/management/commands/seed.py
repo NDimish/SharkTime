f7cbebe33cd500ca38b1bf35e9c21f59dc4a15d8
Justin Lu <justin.lu@kcl.ac.uk>
2022-12-02 16:53:36 +0000

    add seed data

--- lessons/management/commands/seed.py
+++ lessons/management/commands/seed.py
@@ -1,6 +1,8 @@
-from django.core.management.base import BaseCommand, CommandError
+import datetime
+import random
+from lessons.models import Lesson, Teacher, Student
+from django.core.management.base import BaseCommand
 from faker import Faker
-from lessons.models.models import Student
 
 
 class Command(BaseCommand):
@@ -9,43 +11,75 @@ class Command(BaseCommand):
         print("TO DO: Create a seed command following the instructions of the assignment carefully.")
 
 
-# make example
-def faker_data():
-    # make 100 student info
+# Set to global variables
+specialList = ["piano", "violin", "guitar", "guitar", "drums", "saxophone", "cello", "flute"]
+
+
+def seed_Lesson():
+    faker = Faker("en_UK")
+    for name in specialList:
+        lesson = Lesson
+        lesson.name = name
+        lesson.lesson_num = 50  # how many the lesson num suitable?
+        lesson.lesson_price = random("price")
+        lesson.interval = random("interval")
+        lesson.duration = "30,60,90"
+        lesson.description = name
+        lesson.create_time = datetime.datetime.now()
+        lesson.update_time = datetime.datetime.now()
+
+
+def seed_teacher():
+    faker = Faker("en_UK")
+    list_teacher_name = []
+    for i in range(1, 101):
+        teacher = Teacher()
+        teacher.reference_number = faker.ean8()
+        teacher.last_name = faker.last_name()
+        teacher.email = faker.emal()
+        teacher.title = faker.title()
+        teacher.nick_name = faker.company()
+        teacher.special = random_special()
+        teacher.password = faker.password()
+        teacher.create_time = datetime.datetime.now()
+        teacher.update_time = datetime.datetime.now()
+        # insert data to sqlite3 db
+        Teacher.objects.update(teacher)
+
+
+def seed_student():
+    pass
     faker = Faker("en_UK")
     for i in range(1, 101):
-        # geographic information
-        address = faker.address()
-        country = faker.country()
-        country_code = faker.country_code()
-        postcode = faker.postcode()
-
-        # basic info
-        email = faker.email()
-        ssn = faker.ssn()
-        bs = faker.bs()
-        first_name_female = faker.first_name_female()
-        first_name_male = faker.first_name_male()
-        name = faker.name()
-        phone_number = faker.phone_number()
-        credit_card_number = faker.credit_card_number()
-
-        # fake Encrypted information
-        password = faker.password()
-        md5 = faker.md5()
-
-        random_int = faker.random_number(digits=10)
-
-        print(str(random_int))
+        student = Student()
+        student.reference_number = faker.ean8()
+        student.first_name = faker.first_name()
+        student.last_name = faker.last_name()
+        student.birth_date = faker.date_of_birth()
+        student.password = faker.password()
+        student.email = faker.email()
+        student.nick_name = faker.company()
+        student.create_time = datetime.datetime.now()
+        student.update_time = datetime.datetime.now()
+        # insert data to sqlite3 db
+        Student.objects.update(student)
 
 
-def fake_student():
-    pass
-    # faker = Faker("en_UK")
-    # for i in range(1, 101):
-    #     student = Student(
-    #         name=faker.name(),
-    #     )
+def random_price():
+    return round(random.uniform(5, 10), 1)
 
 
-faker_data()
+def random_special():
+    # special list
+    index = random.randint(0, 7)
+    return specialList[index]
+
+
+def random(type):
+    if type == "price":
+        return round(random.uniform(5, 10), 1)
+    if type == "interval":
+        return random.randint(1, 15)
+
+
+random_price()
