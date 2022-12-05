0929056301ccc906da16c16e674cd9056571d858
Sita Sethurajan <k21034097@kcl.ac.uk>
2022-12-05 16:17:09 +0000

    Update admin urls for new pages

--- lessons/Urls/adminUrls.py
+++ lessons/Urls/adminUrls.py
@@ -4,6 +4,9 @@ from ..views import adminViews
 urlpatterns =[
 
     path('home/',adminViews.admin_home, name='adminHome'),
+    path('viewStudents/',adminViews.view_students, name='adminViewStudents'),
+    path('viewRequests/',adminViews.view_requests, name='adminViewRequests'),
+    path('viewTransactions/',adminViews.view_transactions, name='adminViewTransactions'),
     path('<int:id>', adminViews.view_request, name='view_request'),
     path('addBooking/<int:id>/', adminViews.add_booking, name = 'add_booking'),
     path('editBooking/<int:id>/', adminViews.edit_booking, name = 'edit_booking'),
