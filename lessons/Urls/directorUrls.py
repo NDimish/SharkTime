d74347530d28d076dae2eeb8ad19bd96b961cccc
NDimish <nathanmani@outlook.com>
2022-12-08 13:25:11 +0000

    added all models edit from director

--- lessons/Urls/directorUrls.py
+++ lessons/Urls/directorUrls.py
@@ -18,6 +18,20 @@ urlpatterns =[
     path('home/LessonBookingFormPage/<int:Logged_ID>/<int:my_id>',directorViews.LessonBookingFormPage,name='LessonBookingFormPage'),
 
 
+
+    path('home/UserShowPage/<int:Logged_ID>/',directorViews.UserShowPage,name='UserShowPage'),
+    path('home/StudentShowPage/<int:Logged_ID>/',directorViews.StudentShowPage,name='StudentShowPage'),
+    path('home/DirectorShowPage/<int:Logged_ID>/',directorViews.DirectorShowPage,name='DirectorShowPage'),
+    path('home/TeacherShowPage/<int:Logged_ID>/',directorViews.TeacherShowPage,name='TeacherShowPage'),
+    path('home/PaymentShowPage/<int:Logged_ID>/',directorViews.PaymentShowPage,name='PaymentShowPage'),
+    path('home/Sys_userShowPage/<int:Logged_ID>/',directorViews.Sys_userShowPage,name='Sys_userShowPage'),
+    path('home/Sys_authorityShowPage/<int:Logged_ID>/',directorViews.Sys_authorityShowPage,name='Sys_authorityShowPage'),
+    path('home/Sys_user_authorityShowPage/<int:Logged_ID>/',directorViews.Sys_user_authorityShowPage,name='Sys_user_authorityShowPage'),
+    path('home/LessonShowPage/<int:Logged_ID>/',directorViews.LessonShowPage,name='LessonShowPage'),
+    path('home/LessonRequestShowPage/<int:Logged_ID>/',directorViews.LessonRequestShowPage,name='LessonRequestShowPage'),
+    path('home/LessonBookingShowPage/<int:Logged_ID>/',directorViews.LessonBookingShowPage,name='LessonBookingShowPage'),
+
+
     
 
 ]
